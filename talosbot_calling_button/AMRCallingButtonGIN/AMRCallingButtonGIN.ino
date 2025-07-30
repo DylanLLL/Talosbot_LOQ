7ae56c987a3b4cc2a46033fc8797a8cb1f1f4872
DylanLLL <dylanlouislau@gmail.com>
2025-07-30 10:28:01 +0700

    change wifi credentials in calling button GIN

--- talosbot_calling_button/AMRCallingButtonGIN/AMRCallingButtonGIN.ino
+++ talosbot_calling_button/AMRCallingButtonGIN/AMRCallingButtonGIN.ino
@@ -1,28 +1,35 @@
 #define LED1_PIN 25
-#define LED2_PIN 27 
+#define LED2_PIN 27
 #define BTN1_PIN 19
 #define BTN2_PIN 21
 
 #define HOME_WIFI 0
 #define WH_WIFI 0
-#define HOTSPOT_WIFI 1
+#define HOTSPOT_WIFI 0
+#define SARJAY_INCUBUS_WIFI 1
 
 #if HOME_WIFI
-  const char* ssid = "Myhome918";
-  const char* password = "68740109";
-  const char* mqtt_server = "192.168.1.183";
+const char* ssid = "Myhome918";
+const char* password = "68740109";
+const char* mqtt_server = "192.168.1.183";
 #endif
 
 #if HOTSPOT_WIFI
-  const char* ssid = "Dylan";
-  const char* password = "koenigsegg";
-  const char* mqtt_server = "192.168.240.222";
+const char* ssid = "Dylan";
+const char* password = "koenigsegg";
+const char* mqtt_server = "192.168.240.222";
 #endif
 
 #if WH_WIFI
-  const char* ssid = "Incubus";
-  const char* password = "bl1bl1iot";
-  const char* mqtt_server = "10.176.164.12";
+const char* ssid = "Incubus";
+const char* password = "bl1bl1iot";
+const char* mqtt_server = "10.176.164.12";
+#endif
+
+#if SARJAY_INCUBUS_WIFI 
+const char* ssid = "Incubus";
+const char* password = "bl1bl1iot";
+const char* mqtt_server = "10.177.143.122";
 #endif
 
 unsigned long prev_button_BES_press = 0;
@@ -45,12 +52,12 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE	(50)
+#define MSG_BUFFER_SIZE (50)
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
 
-void setup_wifi() {
-
+void setup_wifi()
+{
   delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
@@ -60,12 +67,15 @@ void setup_wifi() {
   WiFi.mode(WIFI_STA);
 
   int try_connect = 0;
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(100);
-    if (try_connect == 0){
+    if (try_connect == 0)
+    {
       WiFi.begin(ssid, password);
     }
-    if (try_connect == 25){
+    if (try_connect == 25)
+    {
       ESP.restart();
     }
     Serial.println("...");
@@ -80,13 +90,13 @@ void setup_wifi() {
 }
 
 // this callback function is like a FEEDBACK that you RECEIVED from the PC
-void callback(char* topic, byte* payload, unsigned int length) {
-
+void callback(char* topic, byte* payload, unsigned int length)
+{
   unsigned long currentMillis = millis();
   Serial.printf("[%lu ms] Received Topic: %s\n", currentMillis, topic);
   Serial.printf("[%lu ms] Received Payload: ", currentMillis);
 
-  //Payload null terminator (\0)
+  // Payload null terminator (\0)
   char terminated_payload[length + 1];
   memcpy(terminated_payload, payload, length);
   terminated_payload[length] = '\0';
@@ -97,85 +107,106 @@ void callback(char* topic, byte* payload, unsigned int length) {
   // }
 
   // //Handle feedback messages
-  if (String(topic) == feedback_button_BES_topic) {
-    if (strcmp(terminated_payload, "in_progress") == 0) {
+  if (String(topic) == feedback_button_BES_topic)
+  {
+    if (strcmp(terminated_payload, "in_progress") == 0)
+    {
       Serial.println("Button BES: Behavior tree is in progress.");
-      digitalWrite(LED1_PIN, HIGH); // Indicate in-progress state
-    } 
-    else if (strcmp(terminated_payload, "success") == 0) {
+      digitalWrite(LED1_PIN, HIGH);  // Indicate in-progress state
+    }
+    else if (strcmp(terminated_payload, "success") == 0)
+    {
       Serial.println("Button BES: Behavior tree succeeded.");
-      digitalWrite(LED1_PIN, LOW); // Turn off LED1
+      digitalWrite(LED1_PIN, LOW);  // Turn off LED1
       task_status = false;
-    } 
-    else if (strcmp(terminated_payload, "failed") == 0) {
+    }
+    else if (strcmp(terminated_payload, "failed") == 0)
+    {
       Serial.println("Button BES: Behavior tree failed.");
       task_status = false;
     }
   }
 
-  if (String(topic) == feedback_button_return_BES_topic) {
-    if (strcmp(terminated_payload, "in_progress") == 0) {
+  if (String(topic) == feedback_button_return_BES_topic)
+  {
+    if (strcmp(terminated_payload, "in_progress") == 0)
+    {
       Serial.println("Button return BES: Behavior tree is in progress.");
-      digitalWrite(LED2_PIN, HIGH); // Indicate in-progress state
-    } 
-    else if (strcmp(terminated_payload, "success") == 0) {
+      digitalWrite(LED2_PIN, HIGH);  // Indicate in-progress state
+    }
+    else if (strcmp(terminated_payload, "success") == 0)
+    {
       Serial.println("Button return BES: Behavior tree succeeded.");
-      digitalWrite(LED2_PIN, LOW); // Turn off LED2
+      digitalWrite(LED2_PIN, LOW);  // Turn off LED2
       task_status = false;
-    } 
-    else if (strcmp(terminated_payload, "failed") == 0) {
+    }
+    else if (strcmp(terminated_payload, "failed") == 0)
+    {
       Serial.println("Button return BES: Behavior tree failed.");
       task_status = false;
     }
   }
 
-  if (String(topic) == feedback_task_status){
-    if (strcmp(terminated_payload, "in_progress") == 0) {
+  if (String(topic) == feedback_task_status)
+  {
+    if (strcmp(terminated_payload, "in_progress") == 0)
+    {
       Serial.println("Task is in progress");
       task_status = true;
-    } 
-    else if (strcmp(terminated_payload, "success") == 0) {
+    }
+    else if (strcmp(terminated_payload, "success") == 0)
+    {
       Serial.println("Task finished");
       task_status = false;
-    } 
-    else if (strcmp(terminated_payload, "failed") == 0) {
+    }
+    else if (strcmp(terminated_payload, "failed") == 0)
+    {
       Serial.println("Task failed");
       task_status = false;
     }
   }
-  if (String(topic) == "/gin/task") {
-  char terminated_payload[length + 1];
-  memcpy(terminated_payload, payload, length);
-  terminated_payload[length] = '\0';
+  if (String(topic) == "/gin/task")
+  {
+    char terminated_payload[length + 1];
+    memcpy(terminated_payload, payload, length);
+    terminated_payload[length] = '\0';
 
-    if (strlen(terminated_payload) > 0) {
+    if (strlen(terminated_payload) > 0)
+    {
       gin_task_received = true;
       Serial.print("Received /gin/task value: ");
       Serial.println(terminated_payload);
-    } else {
+    }
+    else
+    {
       gin_task_received = false;
       Serial.println("Received /gin/task but it's empty");
     }
   }
 }
 
-void reconnect() {
+void reconnect()
+{
   // Loop until we're reconnected
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
-    Serial.print( mqtt_server);
+    Serial.print(mqtt_server);
     // Create a random client ID
     String clientId = "UniqueMQTTClientID";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
-    if (client.connect(clientId.c_str())) {
+    if (client.connect(clientId.c_str()))
+    {
       Serial.println("connected");
       client.subscribe(button_topic.c_str(), 0);
-      client.subscribe(feedback_button_BES_topic.c_str(), 0); // Subscribe to feedback topics
+      client.subscribe(feedback_button_BES_topic.c_str(), 0);  // Subscribe to feedback topics
       client.subscribe(feedback_button_return_BES_topic.c_str(), 0);
       client.subscribe(feedback_task_status.c_str(), 0);
       client.subscribe("/gin/task", 0);
-    } else {
+    }
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 1 second");
@@ -185,7 +216,8 @@ void reconnect() {
   }
 }
 
-void led_blink(){
+void led_blink()
+{
   digitalWrite(LED1_PIN, HIGH);
   digitalWrite(LED2_PIN, HIGH);
   delay(250);
@@ -194,12 +226,12 @@ void led_blink(){
   delay(250);
 }
 
-
-void setup() {
+void setup()
+{
   pinMode(LED1_PIN, OUTPUT);
   pinMode(LED2_PIN, OUTPUT);
   pinMode(BTN1_PIN, INPUT_PULLUP);
-  pinMode(BTN2_PIN, INPUT_PULLUP); 
+  pinMode(BTN2_PIN, INPUT_PULLUP);
 
   Serial.begin(115200);
 
@@ -207,38 +239,44 @@ void setup() {
 
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-
 }
 
-void loop() {
-
-  if (!client.connected()) {
+void loop()
+{
+  if (!client.connected())
+  {
     reconnect();
   }
   client.loop();
-  Serial.print(gin_task_received)
-  unsigned long now = millis();
+  Serial.println(gin_task_received);
+   unsigned long now = millis();
   bool button_BES_read = digitalRead(BTN1_PIN);
   bool button_return_BES_read = digitalRead(BTN2_PIN);
 
-  if (button_BES_read != prev_button_BES_read && task_status == false && gin_task_received){
-    if(millis() - prev_button_BES_press > 250){
-      if(!button_BES_read){
+  if (button_BES_read != prev_button_BES_read && task_status == false && gin_task_received)
+  {
+    if (millis() - prev_button_BES_press > 250)
+    {
+      if (!button_BES_read)
+      {
         String msg = String(1);
-        //String msg = String(1.0) + "," + String(2.0) + "," + String(0.000);
+        // String msg = String(1.0) + "," + String(2.0) + "," + String(0.000);
         client.publish("/button/task", msg.c_str());
         gin_task_received = false;
       }
       prev_button_BES_read = button_BES_read;
-      prev_button_BES_press = millis(); 
+      prev_button_BES_press = millis();
     }
   }
 
-  if (button_return_BES_read != prev_button_return_BES_read && task_status == false && gin_task_received){
-    if(millis() - prev_button_return_BES_press > 250){
-      if(!button_return_BES_read){
+  if (button_return_BES_read != prev_button_return_BES_read && task_status == false && gin_task_received)
+  {
+    if (millis() - prev_button_return_BES_press > 250)
+    {
+      if (!button_return_BES_read)
+      {
         String msg = String(2);
-        //String msg = String(1.0) + "," + String(2.0) + "," + String(0.000);
+        // String msg = String(1.0) + "," + String(2.0) + "," + String(0.000);
         client.publish("/button/task", msg.c_str());
         gin_task_received = false;
       }
@@ -246,4 +284,4 @@ void loop() {
       prev_button_return_BES_press = millis();
     }
   }
-} 
+}
